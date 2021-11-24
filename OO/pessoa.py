12e13aa24d65a6f1802aa8a91779c994d53139fc
WesleyJw <josewesley.silva@ufrpe.br>
2021-11-24 16:57:43 -0300

    Criado objeto complexos filhos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
-    def __init__(self, nome=None):
+    def __init__(self, *filhos, nome=None):
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return "Olá"
 
 if __name__ == '__main__':
-    p = Pessoa("Wesley")
-    print(p.cumprimentar())
+    wesley = Pessoa(nome = "Wesley")
+    jose = Pessoa(wesley, nome = "Jose")
+    print(jose.cumprimentar())
+
+    for filho in jose.filhos:
+        print(filho.nome)
